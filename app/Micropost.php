8130a61c116e5dc669e9b95927c7cb3d08d900ff
Ubuntu <ubuntu@ip-172-31-36-216.ap-northeast-1.compute.internal>
2020-08-03 10:14:53 +0000

    post

---
+++ app/Micropost.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Micropost extends Model
+{
+    protected $fillable = ['content'];
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+}
